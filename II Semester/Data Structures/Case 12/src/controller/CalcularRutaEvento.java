fee8176a41438589829026f5c73217e9d35262b0
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2019-10-21 13:46:56 -0600

    Update - Thread

--- II Semester/Data Structures/Case 12/src/controller/CalcularRutaEvento.java
+++ II Semester/Data Structures/Case 12/src/controller/CalcularRutaEvento.java
@@ -1,16 +1,20 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Queue;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import model.grafo.Grafo;
 import model.grafo.Nodo;
 
-public class CalcularRutaEvento extends MouseAdapter{
+public class CalcularRutaEvento extends MouseAdapter implements IConstants{
 	
 	private ControllerPrincipal controller;
 	
@@ -20,18 +24,39 @@ public class CalcularRutaEvento extends MouseAdapter{
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		
+
+		// Validacion para que exista un origen y un destino
 		if (controller.getNodoOrigen() == null || controller.getNodoDestino()== null) {
-			System.out.println("VACIOS");
+			JOptionPane.showMessageDialog(null, "Debe haber un origen y un destino");
 			return;
 		}
 		
-		Nodo<Point> nodoOrigen = controller.getVista().getTable().get(controller.getNodoOrigen());
-		Nodo<Point> nodoDestino = controller.getVista().getTable().get(controller.getNodoDestino());
+		Hashtable<JLabel, Nodo<Point>> table = controller.getVista().getTable();
 		
-		for(Nodo<Point> actual : controller.getModelo().buscarOrden(nodoOrigen, nodoDestino)) {
-			System.out.println(actual);
-		}
+		Nodo<Point> nodoOrigen = table.get(controller.getNodoOrigen());
+		Nodo<Point> nodoDestino = table.get(controller.getNodoDestino());
+		
+		Queue<Nodo<Point>> puntosRecorridos = controller.getModelo().buscarOrden(nodoOrigen, nodoDestino);
+		
+		
+		// Thread para pintar la ruta con un delay
+		(new Thread(new Runnable(){
+			   public void run(){
+				   for(JLabel actual : controller.getVista().getLabelNodos()) {
+						if (puntosRecorridos.contains(table.get(actual))) {
+							actual.setBackground(Color.ORANGE);
+						}
+						
+						try {
+							Thread.sleep(DELAY);
+						} catch (Exception ex) {
+							
+						}
+						
+					}
+			   }
+			})).start();
+			
 	}
 	
 }
---
+++ II Semester/Data Structures/Case 12/src/controller/IConstants.java
@@ -0,0 +1,5 @@
+package controller;
+
+public interface IConstants {
+	public static int DELAY = 1000; //ms
+}
