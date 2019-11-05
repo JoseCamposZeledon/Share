3cc8067625727d18186f6fc860ba05b349e93723
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2019-11-05 14:56:26 -0600

    Update View

--- II Semester/Data Structures/Final Project/src/view/partida/VistaPartida.java
+++ II Semester/Data Structures/Final Project/src/view/partida/VistaPartida.java
@@ -1,9 +1,32 @@
 package view.partida;
 
-import view.menu.VistaMenu;
+import java.awt.Color;
 
-public class VistaPartida {
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+import view.IConstants;
+
+public class VistaPartida extends JFrame implements IConstants{
+	
+	public VistaPartida() {
+		this.getContentPane().setLayout(null);
+		this.setSize(LARGO + TILE_SIZE, ANCHO + TILE_SIZE);
+		
+		for (int columna = 0; columna <= LARGO / TILE_SIZE; columna++) {
+			for (int fila = 0; fila <= ANCHO / TILE_SIZE; fila++) {
+				
+				JLabel tile = new JLabel(columna + "");
+				tile.setBounds(columna * TILE_SIZE, fila * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+				tile.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				this.add(tile);
+			}
+		}
+		
+		this.setVisible(true);
+	}
+	
 	
 	public static void main(String[] args) {
 		VistaPartida test = new VistaPartida();
