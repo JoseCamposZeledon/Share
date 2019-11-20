a82b05252d0c1c5356caa887334c88b7c2272ee7
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2019-11-20 14:48:39 -0600

    Tablero

---
+++ II Semester/Data Structures/Final Project/src/model/mapComponents/Tablero.java
@@ -0,0 +1,34 @@
+package model.mapComponents;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+public class Tablero extends JLayeredPane implements view.IConstants{
+	
+	public Tablero() {
+		
+		for (int i = 0; i < (MAP_LARGO / TILE_SIZE); i++) {
+			for (int j = 0; j < (MAP_ANCHO/ TILE_SIZE); j++) {
+				MapTile tile;
+				
+				if ((i == 0 || i == 31) && (j == 0 || j == 12 || j == 24)) {
+					tile = new CrownTile();
+				}
+				else if ((i == 1 || i ==30) && (j == 0 || j == 12 || j == 24)) {
+					tile = new GroupTile();
+				}
+				else {
+					tile = new MapTile();
+				}
+				
+				tile.setBounds(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+				
+				this.add(tile, 0);
+			}
+		}
+	}
+	
+}
